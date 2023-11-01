265224228c99e5526702af141b14c4e8bd8f8d4c
Joey Smith <jsmith@webinertia.net>
2023-11-01 11:20:48 -0500

    updating ActionMiddleware Signed-off-by: Joey Smith <jsmith@webinertia.net>
    Signed-off-by: Joey Smith <jsmith@webinertia.net>

--- src/AutoManager/src/Middleware/ActionMiddleware.php
+++ src/AutoManager/src/Middleware/ActionMiddleware.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace AutoManager\Middleware;
 
 use AutoManager\Storage\Repository\ManufacturerRepository;
+use Fig\Http\Message\StatusCodeInterface as RequestStatus;
+use Laminas\Diactoros\Response\EmptyResponse;
 use Laminas\Diactoros\Response\HtmlResponse;
 use Mezzio\Template\TemplateRendererInterface;
 use Psr\Http\Message\ResponseInterface;
@@ -20,17 +22,34 @@ class ActionMiddleware implements MiddlewareInterface
     ) {
 
     }
-    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler) : ResponseInterface
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        $action = $request->getAttribute('action', 'index');
+        $action = $request->getAttribute('action');
         return match($action) {
+            'add'   => $this->addAction($request),
             'index' => $this->indexAction($request),
             default => $this->indexAction($request),
         };
     }
 
-    private function indexAction(ServerRequestInterface $request)
+    private function addAction(ServerRequestInterface $request)
     {
+        $attribs = $request->getAttributes();
+        //$post    = $request->
+        return match($attribs['type']) {
+            'manufacturer', 'model', 'vehicle' => new HtmlResponse(
+                $this->template->render('auto-manager::add-' . $attribs['type'] .'.phtml')
+            ),
+            default => new EmptyResponse(RequestStatus::STATUS_NOT_FOUND),
+        };
+    }
+
+    private function indexAction(ServerRequestInterface $request): ResponseInterface
+    {
+        $attribs = $request->getAttributes();
+        return match($attribs['type']) {
+
+        };
         return new HtmlResponse($this->template->render('auto-manager::index'));
     }
 }
